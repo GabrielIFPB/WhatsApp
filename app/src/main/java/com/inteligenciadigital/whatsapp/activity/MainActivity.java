ed8fa3ed218748b2ca35e488314ffd4f1c112fa4
gabriel <jeronimogbariel@gmail.com>
2020-07-11 16:24:41 -0300

    layout de login criado

--- app/src/main/java/com/inteligenciadigital/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/inteligenciadigital/whatsapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.inteligenciadigital.whatsapp;
+package com.inteligenciadigital.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.inteligenciadigital.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
