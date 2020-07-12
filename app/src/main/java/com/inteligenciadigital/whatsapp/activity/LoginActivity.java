2453329d880abd220e50fa0e70c8da14c2de7060
gabriel <jeronimogbariel@gmail.com>
2020-07-12 18:58:23 -0300

    layout cadastro estilo ok

--- app/src/main/java/com/inteligenciadigital/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/inteligenciadigital/whatsapp/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.inteligenciadigital.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.inteligenciadigital.whatsapp.R;
 
@@ -12,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+	}
+
+	public void logar(View view)  {
+
+	}
+
+	public void getActivityCadastrar(View view) {
+		startActivity(new Intent(this, CadastroActivity.class));
 	}
 }
